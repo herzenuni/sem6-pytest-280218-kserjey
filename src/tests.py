14df8ec1e8700a55c9abcb4ae666d1f79409f8e6
Sergey Korchinskiy <k.serjey@gmail.com>
2018-03-28 12:24:23 +0300

    Add hypothesis test

--- src/tests.py
+++ src/tests.py
@@ -1,4 +1,6 @@
 import pytest
+from hypothesis import given
+import hypothesis.strategies as st
 from main import create_dict
 
 def test_default():
@@ -12,6 +14,14 @@ def test_fill_none():
     keys = ['nick', 'goss', 'pasha']
     values = ['python', 'nodejs']
     expected = { 'nick': 'python', 'goss': 'nodejs', 'pasha': None }
+
+    assert create_dict(keys, values) == expected
+
+@given(st.lists(st.text()))
+def test_skip_values(extra_values):
+    keys = ['nick', 'goss']
+    values = ['python', 'nodejs'] + extra_values
+    expected = { 'nick': 'python', 'goss': 'nodejs' }
 
     assert create_dict(keys, values) == expected
 
